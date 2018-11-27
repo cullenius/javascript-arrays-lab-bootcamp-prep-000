ba1a79633e98a91746d2da4509c8043de2075dcb
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-27 16:35:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,5 +30,10 @@ function prependKitten(name){
 }
 
 function removeLastKitten(name){
-  kittens.slice(0, kittens.length - 1)
+  kittens.slice(0, kittens.length - 1);
+  return kittens;
 }
+
+function removeFirstKitten(){
+  
+} 
